14b148cbc27ccb93d289ec0eb6a7f0f9b6f3a2c8
farkhodsadykov <sadykovfarkhod@gmail.com>
2019-02-08 20:36:38 -0600

    Fixed slurper

--- src/com/lib/jsonviewer.groovy
+++ src/com/lib/jsonviewer.groovy
@@ -2,7 +2,8 @@ import groovy.json.JsonSlurper
 import groovy.json.JsonBuilder
 
 def showUserInfo(jsonData) {
-  def data = new JsonSlurper().parse(jsonData)
+  def slurper = new JsonSlurper()
+  def data = new slurper.parse(new File("json/data.json"))
   data.each() {
     println("Users first name :${it['first_name']}, Last name :${it['last_name']}")
   }

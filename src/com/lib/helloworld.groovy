e5a315690a888842c9b9a24c9925b1e4d4a442ea
farkhodsadykov <sadykovfarkhod@gmail.com>
2019-02-08 18:32:56 -0600

    Created result function

--- src/com/lib/helloworld.groovy
+++ src/com/lib/helloworld.groovy
@@ -1,4 +1,28 @@
+#!/usr/bin/env groovy
 package com.lib
+
+import groovy.json.*
+
+
+
 def hello() {
   println("Hello World")
+}
+
+
+
+def result(data) {
+
+  def jsonSlurper = new JsonSlurper()
+  def reader = new BufferedReader(
+  new InputStreamReader(
+    new FileInputStream(data),"UTF-8")
+    )
+    
+  resultData = jsonSlurper.parse(reader)
+  if (resultData.data) {
+    resultData.data.each() {
+      println("Users first name :${it['first_name']}, Last name :${it['last_name']}")
+    }
+  }
 }

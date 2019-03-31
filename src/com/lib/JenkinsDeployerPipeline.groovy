7e7270c092b730eb158620fd869449622217b75b
Farkhod <sadykovfarkhod@gmail.com>
2019-03-31 02:07:47 -0500

    Fixed branch name $

--- src/com/lib/JenkinsDeployerPipeline.groovy
+++ src/com/lib/JenkinsDeployerPipeline.groovy
@@ -2,7 +2,7 @@
 package com.lib
 
 def runPipeline() {
-  def branchName = ${JOB_NAME}.slplit('/').last()
+  def branchName = "${JOB_NAME}".slplit('/').last()
   branch = "${scm.branches[0].name}".replaceAll(/^\*\//, '').replace("/", "-").toLowerCase()
   println("${branch}")
 }

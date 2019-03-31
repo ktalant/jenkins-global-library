e35b5ec9ba371de61951d1f1e4f435a6ca9d37dd
Farkhod <sadykovfarkhod@gmail.com>
2019-03-31 00:10:40 -0500

    Deleted functions

--- src/com/lib/helloworld.groovy
+++ src/com/lib/helloworld.groovy
@@ -9,19 +9,3 @@ import groovy.json.JsonBuilder
 def hello() {
   println("Hello World")
 }
-
-def result(data) {
-
-  def jsonSlurper = new JsonSlurper()
-  def reader = new BufferedReader(
-  new InputStreamReader(
-    new FileInputStream(data),"UTF-8")
-    )
-
-  resultData = jsonSlurper.parse(reader)
-  
-  resultData.data.each() {
-    println("Users first name :${it['first_name']}, Last name :${it['last_name']}")
-  }
-
-}

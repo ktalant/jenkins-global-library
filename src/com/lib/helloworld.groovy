eea451e3856f9c905ba89c5f3c92ea922bef2598
Farkhod <sadykovfarkhod@gmail.com>
2019-03-31 00:19:35 -0500

    Deleted json stuf

--- src/com/lib/helloworld.groovy
+++ src/com/lib/helloworld.groovy
@@ -1,15 +1,10 @@
 #!/usr/bin/env groovy
 package com.lib
 
-// import groovy.json.*
-
-import groovy.json.JsonSlurper
-import groovy.json.JsonBuilder
 
 def hello() {
   println("Hello World")
 }
-
 
 
 return this

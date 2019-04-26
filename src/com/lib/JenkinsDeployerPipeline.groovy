b16d26385d21cdfe553dbd426690d65663052ecb
Farkhod <sadykovfarkhod@gmail.com>
2019-04-25 19:25:51 -0500

    Variables moved into function

--- src/com/lib/JenkinsDeployerPipeline.groovy
+++ src/com/lib/JenkinsDeployerPipeline.groovy
@@ -2,9 +2,7 @@
 package com.lib
 import groovy.json.JsonSlurper
 
-def salckChannel = 'test-message'
-slackUrl = 'https://fuchicorp.slack.com/services/hooks/jenkins-ci/'
-slackTokenId = 'slack-token'
+
 
 
 
@@ -13,6 +11,9 @@ def runPipeline() {
 
   def environment = ""
   def branch = "${scm.branches[0].name}".replaceAll(/^\*\//, '').replace("/", "-").toLowerCase()
+  def salckChannel = 'test-message'
+  slackUrl = 'https://fuchicorp.slack.com/services/hooks/jenkins-ci/'
+  slackTokenId = 'slack-token'
 
   switch(branch) {
     case 'master': environment = 'prod'

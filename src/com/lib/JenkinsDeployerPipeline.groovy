f9e7a806d82b4ae24adecc5b42e9796ef11b364f
Farkhod <sadykovfarkhod@gmail.com>
2019-05-25 03:09:25 -0500

    Checking

--- src/com/lib/JenkinsDeployerPipeline.groovy
+++ src/com/lib/JenkinsDeployerPipeline.groovy
@@ -57,6 +57,7 @@ def runPipeline() {
 
       stage('Terraform init') {
         dir("${WORKSPACE}/deployment/terraform") {
+          sh "ls"
           sh "source get-setenv.sh"
         }
       }

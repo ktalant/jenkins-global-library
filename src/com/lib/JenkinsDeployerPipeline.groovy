869363f4444e1bd0ebd3e4157257a1d5f66a9fbc
Farkhod <sadykovfarkhod@gmail.com>
2019-05-31 09:31:56 -0500

    Checking for FAILURE

--- src/com/lib/JenkinsDeployerPipeline.groovy
+++ src/com/lib/JenkinsDeployerPipeline.groovy
@@ -28,89 +28,97 @@ def runPipeline() {
         print('This branch does not supported')
   }
 
-  node('master') {
-    properties([ parameters([
-      choice(name: 'SelectedDockerImage', choices: findDockerImages(branch), description: 'Please select docker image to deploy!'),
-      booleanParam(defaultValue: false, description: 'Apply All Changes', name: 'terraformApply'),
-      booleanParam(defaultValue: false, description: 'Destroy deployment', name: 'terraformDestroy'),
-      string( defaultValue: 'webplatform', name: 'mysql_database', value: 'dbwebplatform', description: 'Please enter database name'),
-      string(defaultValue: 'webplatformUser',  name: 'mysql_user',description: 'Please enter a username for MySQL', trim: true),
-      string(defaultValue: 'webplatformPassword',  name: 'mysql_password',description: 'Please enter a password for MySQL', trim: true)
+  try {
+      node('master') {
+        properties([ parameters([
+          choice(name: 'SelectedDockerImage', choices: findDockerImages(branch), description: 'Please select docker image to deploy!'),
+          booleanParam(defaultValue: false, description: 'Apply All Changes', name: 'terraformApply'),
+          booleanParam(defaultValue: false, description: 'Destroy deployment', name: 'terraformDestroy'),
+          string( defaultValue: 'webplatform', name: 'mysql_database', value: 'dbwebplatform', description: 'Please enter database name'),
+          string(defaultValue: 'webplatformUser',  name: 'mysql_user',description: 'Please enter a username for MySQL', trim: true),
+          string(defaultValue: 'webplatformPassword',  name: 'mysql_password',description: 'Please enter a password for MySQL', trim: true)
 
-      ]
-      )])
+          ]
+          )])
 
-      checkout scm
-      notifyStarted()
+          checkout scm
+          notifyStarted()
 
-      stage('Generate Vars') {
-        def file = new File("${WORKSPACE}/deployment/terraform/webplatform.tfvars")
-        file.write """
-        mysql_user                =  "${mysql_user}"
-        mysql_database            =  "${mysql_database}"
-        mysql_host                =  "webplatform-mysql"
-        webplatform_namespace     =  "${environment}"
-        webplatform_password      =  "${mysql_password}"
-        webplatform_image         =  "docker.fuchicorp.com/${SelectedDockerImage}"
-        environment               =  "${environment}"
-        """
-      }
+          stage('Generate Vars') {
+            def file = new File("${WORKSPACE}/deployment/terraform/webplatform.tfvars")
+            file.write """
+            mysql_user                =  "${mysql_user}"
+            mysql_database            =  "${mysql_database}"
+            mysql_host                =  "webplatform-mysql"
+            webplatform_namespace     =  "${environment}"
+            webplatform_password      =  "${mysql_password}"
+            webplatform_image         =  "docker.fuchicorp.com/${SelectedDockerImage}"
+            environment               =  "${environment}"
+            """
+          }
 
-      stage('Terraform init') {
-        dir("${WORKSPACE}/deployment/terraform") {
-          sh "terraform init"
-        }
-      }
-
-      stage('Terraform Apply/Plan') {
-        if (!params.terraformDestroy) {
-          if (params.terraformApply) {
-
+          stage('Terraform init') {
             dir("${WORKSPACE}/deployment/terraform") {
-              echo "##### Terraform Applying the Changes ####"
-              sh "terraform apply --auto-approve -var-file=webplatform.tfvars"
-            }
-
-          } else {
-
-              dir("${WORKSPACE}/deployment/terraform") {
-                echo "##### Terraform Plan (Check) the Changes ####"
-                sh "terraform plan -var-file=webplatform.tfvars"
-              }
-
-          }
-        }
-      }
-      stage('Terraform Destroy') {
-        if (!params.terraformApply) {
-          if (params.terraformDestroy) {
-            if ( branch == 'dev' || branch == 'qa' ) {
-              dir("${WORKSPACE}/deployment/terraform") {
-                echo "##### Terraform Destroing ####"
-                sh "terraform destroy --auto-approve -var-file=webplatform.tfvars"
-              }
-            } else {
-              println("""
-
-                Sorry I can not destroy PROD!!!
-                I can Destroy only dev and qa branch
-
-              """)
+              sh "terraform init"
             }
           }
-       }
 
-       if (params.terraformDestroy) {
-         if (params.terraformApply) {
-           println("""
-           Sorry you can not destroy and apply at the same time
-           """)
+          stage('Terraform Apply/Plan') {
+            if (!params.terraformDestroy) {
+              if (params.terraformApply) {
+
+                dir("${WORKSPACE}/deployment/terraform") {
+                  echo "##### Terraform Applying the Changes ####"
+                  sh "terraform apply --auto-approve -var-file=webplatform.tfvars"
+                }
+
+              } else {
+
+                  dir("${WORKSPACE}/deployment/terraform") {
+                    echo "##### Terraform Plan (Check) the Changes ####"
+                    sh "terraform plan -var-file=webplatform.tfvars"
+                  }
+
+              }
+            }
+          }
+          stage('Terraform Destroy') {
+            if (!params.terraformApply) {
+              if (params.terraformDestroy) {
+                if ( branch == 'dev' || branch == 'qa' ) {
+                  dir("${WORKSPACE}/deployment/terraform") {
+                    echo "##### Terraform Destroing ####"
+                    sh "terraform destroy --auto-approve -var-file=webplatform.tfvars"
+                  }
+                } else {
+                  println(""" ${data}
+
+                    Sorry I can not destroy PROD!!!
+                    I can Destroy only dev and qa branch
+
+                  """)
+                }
+              }
+           }
+
+           if (params.terraformDestroy) {
+             if (params.terraformApply) {
+               println("""
+               Sorry you can not destroy and apply at the same time
+               """)
+             }
          }
+       }
+       // If after finished everthing will send notification to slack
+       notifySuccessful()
      }
-   }
-   // If after finished everthing will send notification to slack
-   notifySuccessful()
- }
+
+  } catch (e) {
+    currentBuild.result = 'FAILURE'
+    println("ERROR Detected:")
+    println(e.getMessage())
+    notifyFailed()
+  }
 }
 
 def findDockerImages(branchName) {

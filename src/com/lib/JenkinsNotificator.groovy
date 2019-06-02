3993029370df12619b705eb4383db41d61853c46
Farkhod <sadykovfarkhod@gmail.com>
2019-06-02 13:59:28 -0500

    Testing with optional argument

--- src/com/lib/JenkinsNotificator.groovy
+++ src/com/lib/JenkinsNotificator.groovy
@@ -3,8 +3,9 @@
 package com.lib
 
 
-def sendMessage(String type, String message, String status, String channel) {
+def sendMessage(String type, String message = null, String status, String channel) {
   String color        = ""
+  String message      = ""
   String slackUrl     = "https://fuchicorp.slack.com/services/hooks/jenkins-ci/"
   String slackToken   = "slack-token"
 
@@ -15,14 +16,36 @@ def sendMessage(String type, String message, String status, String channel) {
   switch(status) {
     case "SUCCESS":
       color = "#00FF00"
+      if (message != null ) {
+        message = """
+        Jenkins Job was successfully built.
+        email: fuchicorpsolution@gmail.com
+        SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})
+        """)
+      }
+
       break
 
     case "FAILURE":
       color = "#FF0000"
+      if (message != null ) {
+        message = """
+        Jenkins build is breaking for some reason. Please go to job and take actions.
+        email: fuchicorpsolution@gmail.com
+        FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+        """
+      }
       break
 
     default:
       color = "#FFFF00"
+      if (message != null ) {
+        message =  """
+        Please add let team know if this is mistake or please send an email
+        email: fuchicorpsolution@gmail.com
+        STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL}).
+        """
+      }
       break
   }
 

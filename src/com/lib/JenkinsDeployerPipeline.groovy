bd23440494371d2665f693cd5cb9e1111355afd0
Farkhod <sadykovfarkhod@gmail.com>
2019-06-05 16:55:41 -0500

    Fixed findDockerImages for all docker images

--- src/com/lib/JenkinsDeployerPipeline.groovy
+++ src/com/lib/JenkinsDeployerPipeline.groovy
@@ -120,21 +120,36 @@ def runPipeline() {
   }
 }
 
+import groovy.json.JsonSlurper
+
+
 def findDockerImages(branchName) {
   def versionList = []
+  def token       = ""
   def myJsonreader = new JsonSlurper()
-  def nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=webplatform"))
-  nexusData.items.each {
-    if (it.name.contains(branchName)) {
-       versionList.add(it.name + ':' + it.version)
-     }
+  while (true) {
+    def nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=webplatform"))
+    if (nexusData.continuationToken) {
+      nexusData.items.each {
+        if (it.name.contains(branchName)) {
+           versionList.add(it.name + ':' + it.version)
+         }
+        }
+
+      token = nexusData.continuationToken
+      nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=webplatform&continuationToken=${token}"))
+      nexusData.items.each {
+        if (it.name.contains(branchName)) {
+           versionList.add(it.name + ':' + it.version)
+         }
+        }
+
+    }
+    if (nexusData.continuationToken == null ){
+      break
     }
 
-
-  if (versionList.isEmpty()) {
-    return ['ImageNotFound']
   }
-
   return versionList
 }
 

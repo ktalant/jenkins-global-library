080c1925ade97bcb0fbb759f7a9a2f53f61b6049
Farkhod <sadykovfarkhod@gmail.com>
2019-06-07 03:04:27 -0500

    Fixed to cat

--- src/com/lib/JenkinsDeployerPipeline.groovy
+++ src/com/lib/JenkinsDeployerPipeline.groovy
@@ -80,7 +80,7 @@ def runPipeline() {
 
                   dir("${WORKSPACE}/deployment/terraform") {
                     echo "##### Terraform Plan (Check) the Changes #####"
-                    sh "./webplatform.tfvars"
+                    sh "cat ./webplatform.tfvars"
                     sh '''#!/bin/bash -e
                     source set-env.sh ./webplatform.tfvars
                     terraform plan -var-file=$DATAFILE'''

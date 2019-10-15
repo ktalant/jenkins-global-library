063d65ffec08b8a78b0fdc5e488adc74a441bf6c
Farkhod <sadykovfarkhod@gmail.com>
2019-10-14 20:41:55 -0500

    Checking from old version

--- src/com/lib/JenkinsCommonDeployPipeline.groovy
+++ src/com/lib/JenkinsCommonDeployPipeline.groovy
@@ -30,52 +30,47 @@ def runPipeline() {
 
 
 
-    def slavePodTemplate = """
-    metadata:
-      labels:
-        k8s-label: ${k8slabel}
-      annotations:
-        jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
-    spec:
-      affinity:
-        podAntiAffinity:
-          requiredDuringSchedulingIgnoredDuringExecution:
-          - labelSelector:
-              matchExpressions:
-              - key: component
-                operator: In
-                values:
-                - jenkins-jenkins-master
-            topologyKey: "kubernetes.io/hostname"
-      containers:
-      - name: docker
-        image: docker:latest
-        imagePullPolicy: Always
-        tty: true
-        command:
-        - cat
-        volumeMounts:
-          - mountPath: /etc/secrets/service-account/
-            name: google-service-account
-          - mountPath: /var/run/docker.sock
-            name: docker-sock
-      - name: fuchicorptools
-        image: fuchicorp/buildtools
-        imagePullPolicy: Always
-        tty: true
-        command:
-        - cat
-      securityContext:
-        runAsUser: 0
-        fsGroup: 0
-      volumes:
-      - name: google-service-account
-        secret:
-          secretName: fuchicorp-service-account
-        - name: docker-sock
-          hostPath:
-            path: /var/run/docker.sock
-  """
+      def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+        - name: docker
+          image: docker:latest
+          imagePullPolicy: Always
+          command:
+          - cat
+          tty: true
+          volumeMounts:
+            - mountPath: /var/run/docker.sock
+              name: docker-sock
+        - name: fuchicorptools
+          image: fuchicorp/buildtools
+          imagePullPolicy: Always
+          command:
+          - cat
+          tty: true
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+    """
 
   podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate) {
       node(k8slabel) {

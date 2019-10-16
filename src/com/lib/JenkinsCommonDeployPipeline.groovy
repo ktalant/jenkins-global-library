a0d4483f9d2058bbc3cfcb0bc7f3f597e6045445
Farkhod <sadykovfarkhod@gmail.com>
2019-10-16 06:09:54 -0500

    checking with .stripIndent()

--- src/com/lib/JenkinsCommonDeployPipeline.groovy
+++ src/com/lib/JenkinsCommonDeployPipeline.groovy
@@ -117,7 +117,7 @@ def runPipeline() {
             deployment_name        = \"${deploymentName}\"
             deployment_environment = \"${environment}\"
             deployment_image       = \"docker.fuchicorp.com/${selectedDockerImage}\"
-            """
+            """.stripIndent()
 
 
 

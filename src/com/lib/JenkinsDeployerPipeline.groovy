618375f1c7e75425409113ee18ca4cb38669b7a9
Farkhod <sadykovfarkhod@gmail.com>
2019-11-06 03:40:30 -0600

    Fixed repo location

--- src/com/lib/JenkinsDeployerPipeline.groovy
+++ src/com/lib/JenkinsDeployerPipeline.groovy
@@ -21,7 +21,7 @@ def runPipeline() {
                         .replace('-fuchicorp', '')
                         .replace('-build', '')
                         .replace('-deploy', '')
-  def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"               
+  def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
   def slavePodTemplate = """
   metadata:
     labels:
@@ -214,7 +214,7 @@ def findDockerImages(branchName) {
   def versionList = []
   def token       = ""
   def myJsonreader = new JsonSlurper()
-  def nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=webplatform"))
+  def nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=fuchicorp"))
   nexusData.items.each {
     if (it.name.contains(branchName)) {
        versionList.add(it.name + ':' + it.version)
@@ -223,7 +223,7 @@ def findDockerImages(branchName) {
   while (true) {
     if (nexusData.continuationToken) {
       token = nexusData.continuationToken
-      nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=webplatform&continuationToken=${token}"))
+      nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=fuchicorp&continuationToken=${token}"))
       nexusData.items.each {
         if (it.name.contains(branchName)) {
            versionList.add(it.name + ':' + it.version)
--- src/com/lib/webplatform-deployment.groovy
+++ src/com/lib/webplatform-deployment.groovy
@@ -162,7 +162,7 @@ def findDockerImages(branchName) {
   def versionList = []
   def token       = ""
   def myJsonreader = new JsonSlurper()
-  def nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=webplatform"))
+  def nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=fuchicorp"))
   nexusData.items.each {
     if (it.name.contains(branchName)) {
        versionList.add(it.name + ':' + it.version)
@@ -171,7 +171,7 @@ def findDockerImages(branchName) {
   while (true) {
     if (nexusData.continuationToken) {
       token = nexusData.continuationToken
-      nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=webplatform&continuationToken=${token}"))
+      nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=fuchicorp&continuationToken=${token}"))
       nexusData.items.each {
         if (it.name.contains(branchName)) {
            versionList.add(it.name + ':' + it.version)

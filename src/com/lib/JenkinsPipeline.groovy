fe800e5db443408b5334b7ec6a0d57215dc85912
farrukh90 <farrukhsadykov@gmail.com>
2020-01-25 23:37:29 -0600

    add package com.lib
    def runPipeline(){
        properties([parameters([choice(choices: ['1,', '2,', '3,', '4'], description: 'Give me that ', name: 'Choice1')])])
        echo "Hello World"
    }

--- src/com/lib/JenkinsPipeline.groovy
+++ src/com/lib/JenkinsPipeline.groovy
@@ -2,5 +2,6 @@
 package com.lib
 
 def runPipeline(){
+    properties([parameters([choice(choices: ['1,', '2,', '3,', '4'], description: 'Give me that ', name: 'Choice1')])])
     echo "Hello World"
 }
